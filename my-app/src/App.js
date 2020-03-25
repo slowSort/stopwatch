8fd0acd67edc9483e187146cb0e68b625c1bc741
Daniel M Gibbons <daniel.gibbons@skyscanner.net>
2020-03-25 19:31:48 +0000

    Add controlled form with field validation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,13 +13,51 @@ function App() {
       <header className="App-header">
       <h1>Custom Stopwatch</h1>
       {ReturnStopwatches()}
-      <form>
-        <input type="text" id="name" name="name"/>
-        <input type="submit" value="Add"/>
-      </form>
+      <NameForm></NameForm>
       </header>
     </div>
   );
+}
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stopwatchName: '',
+      stopwatchTime: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    this.setState({
+      [name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.stopwatchName + ' And a time: ' + this.state.stopwatchTime);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input name="stopwatchName" type="text" value={this.state.stopwatchName} onChange={this.handleChange} />
+          </label>
+          <label>
+            Time:
+            <input name="stopwatchTime" type="number" value={this.state.stopwatchTime} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+    );
+  }
 }
 
 function ReturnStopwatches() {
